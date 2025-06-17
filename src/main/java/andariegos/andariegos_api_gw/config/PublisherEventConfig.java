1cff509ad8c63ee39b05fed15c4db935fd8a633f
SimonRamos88 <joramos@unal.edu.co>
2025-06-16 20:58:03 -0500

    rabbit mq persistente

--- src/main/java/andariegos/andariegos_api_gw/config/PublisherEventConfig.java
+++ src/main/java/andariegos/andariegos_api_gw/config/PublisherEventConfig.java
@@ -1,15 +1,24 @@
 package andariegos.andariegos_api_gw.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.core.Queue;
 
+import andariegos.andariegos_api_gw.filters.ReportFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 
 @Configuration
 public class PublisherEventConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(PublisherEventConfig.class);
 
     @Value("${NAME_QUEUE}")
     private String queueName;
@@ -19,4 +28,17 @@ public class PublisherEventConfig {
         return new Queue(queueName, true);
     }
     
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        log.info("inicializado ");
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationRunner declareQueue(RabbitAdmin rabbitAdmin, Queue queue) {
+        return args -> {
+            rabbitAdmin.declareQueue(queue);
+            System.out.println(" Queue '" + queue.getName() + "' declared manually by ApplicationRunner");
+        };
+    }
 }
